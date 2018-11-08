aa1fd19bc691d1e1875cdda59c46d4c840f3fc2a
Simonov Mihail <Mihail.Jaxak.Simonov@gmail.com>
2018-11-08 10:22:00 +0500

    чекается и ставятся все даты

--- js/popup.js
+++ js/popup.js
@@ -23,3 +23,29 @@ function addPopupDiv() {
         $('div.window-overlay').prepend(data);
     });
 }
+
+$('div.window').mouseenter(function () {
+    CheckBoxStatus("testingstatusbox1");
+    CheckBoxStatus("testingstatusbox2");
+    CheckBoxStatus("testingstatusbox3");
+    CheckBoxStatus("testingstatusbox4");
+});
+
+
+$(document).mouseup(function (e) {
+    var container = $("#modalMyPopUp");
+    if (container.has(e.target).length === 0) {
+        container.hide();
+    }
+});
+
+function CheckBoxStatus(idCheckbox) {
+    $("#"+idCheckbox).one('click', 'div.checklist-item-checkbox', function () {
+        $("#"+idCheckbox).toggleClass("checklist-item-state-complete");
+        getDate(idCheckbox);
+    });
+}
+
+function getDate(idCheckbox) {
+    $("input."+idCheckbox).val(getCurrentDate());
+}
--- js/script.js
+++ js/script.js
@@ -4,29 +4,3 @@ $('div.window').mouseenter(function () {
     } else {
     }
 });
-
-$(document).mouseenter(function () {
-    CheckBoxStatus("#testingstatusbox1");
-    CheckBoxStatus("#testingstatusbox2");
-    CheckBoxStatus("#testingstatusbox3");
-    CheckBoxStatus("#testingstatusbox4");
-});
-
-$(document).mouseup(function (e) {
-    var container = $("#modalMyPopUp");
-    if (container.has(e.target).length === 0) {
-        container.hide();
-    }
-});
-
-function CheckBoxStatus(idCheckbox) {
-    $(idCheckbox).on('click', 'div.checklist-item-checkbox', function () {
-        $(idCheckbox).toggleClass("checklist-item-state-complete");
-        getDate(idCheckbox);
-    });
-}
-
-function getDate(idCheckbox) {
-    $("input.datecheck").val(getCurrentDate());
-
-}
